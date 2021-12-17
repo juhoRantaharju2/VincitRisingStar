4e4401a1c2465b080526b02d68e5475b881d8632
Juho Rantaharju <juho.rantaharju2@gmail.com>
2021-12-17 17:07:06 +0200

    going forward

--- vincitrisingstar/src/workingonit.js
+++ vincitrisingstar/src/workingonit.js
@@ -102,3 +102,33 @@ function logi() {
   //console.log(startDate.getMilliseconds());
 
 }
+
+const submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", logi);
+const start = document.getElementById("start");
+
+const url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=1392577232&to=1422577232";
+
+async function getData(url){
+
+ const response = await fetch(url);
+ let data = await response.json();
+ console.log(data);
+
+}
+
+getData(url);
+
+function logi() {
+
+	let startDate = new Date(start.value);
+
+	console.log("text");
+  //console.log(response);
+  console.log(start.value);
+  //startDate = start.value;
+  console.log(startDate.getDay());
+  
+  //console.log(startDate.getMilliseconds());
+
+}
