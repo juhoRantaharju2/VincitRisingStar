d2a0852736a0423b23229f79ead96ed859d1ea0e
Juho Rantaharju <juho.rantaharju2@gmail.com>
2021-12-17 22:57:52 +0200

    trying stuff for chart

--- vincitrisingstar/src/workingonit.js
+++ vincitrisingstar/src/workingonit.js
@@ -229,3 +229,11 @@ function logi() {
 <button id="submit" >
   longest down
 </button>*/
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
+
+let graphs = new Chart(mychart, {
+  type: "bar",
+  data: {da},
+  options: {}
+});
