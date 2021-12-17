b24323bc6c1c0a11ceecc08c4c1fc91d4a152845
Juho Rantaharju <juho.rantaharju2@gmail.com>
2021-12-17 14:26:49 +0200

    trying out things. Working on getting date in unix timestamp

--- vincitrisingstar/src/workingonit.js
+++ vincitrisingstar/src/workingonit.js
@@ -1,5 +1,6 @@
 const submitButton = document.getElementById("submit");
 submitButton.addEventListener("click", logi);
+const start = document.getElementById("start");
 
 let url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=1392577232&to=1422577232";
 
@@ -17,8 +18,21 @@ function logi() {
 
 	console.log("text");
   //console.log(response);
+  console.log(start.value);
 
 }
-
 //This is for a unix timestamp that needs to be send
 let ts = Math.round((new Date()).getTime() / 1000);
+
+/*
+<label>Start date:</label>
+<input type="date" id="start" value="">
+<br />
+<label>End date:</label>
+<input type="date" id="end" value="">
+<br />
+
+<button id="submit" >
+  longest down
+</button>
+*/
